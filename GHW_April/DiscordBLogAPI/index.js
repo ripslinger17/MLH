a1e9577950335a79eec1beb4ca67013817d1a7a1
Smit <smit.p1707@gmail.com>
2023-04-07 10:54:13 +0530

    Global Hack Week:APIs

---
+++ GHW_April/DiscordBLogAPI/index.js
@@ -0,0 +1,48 @@
+const PORT = process.env.PORT || 8000;
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+const dblog = [
+  {
+    name : 'Discord-Blogs',
+    address: 'https://discord.com/blog',
+    base: 'https://discord.com/blog'
+  }
+];
+
+const articles = [];
+
+dblog.forEach(blog => {
+  axios.get(blog.address)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html)
+
+      $('a:contains("Discord")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr('href');
+
+        articles.push({
+          title,
+          url: blog.base + url,
+          source : blog.name
+        });
+      });
+      
+    });
+});
+
+app.get('/', (req,res) => {
+  res.json('Welcome to the Discord Blog API');
+});
+
+app.get('/articles', (req,res) => {
+  res.json(articles);
+});
+
+
+
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
